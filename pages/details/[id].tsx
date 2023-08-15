104135df795f51defda3f0a0a48ea6ebad0d7099
Etumnu Prosper <johnikechukwu94@gmail.com>
2023-08-15 07:03:42 +0100

    update fix

--- pages/details/[id].tsx
+++ pages/details/[id].tsx
@@ -2,22 +2,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 
-type location = {
-  name: string;
-};
-
-type characterType = {
-  name: string;
-  image: string;
-  status: string;
-  id: number;
-  species: string;
-  created: string;
-  gender: string;
-  location: location;
-  episodeCount: number;
-};
-
 const Details = () => {
   const [getData, setGetData] = useState<characterType>();
 
@@ -40,43 +24,49 @@ const Details = () => {
     getdata();
   }, []);
 
-  const charactersEpisodeCode = (arr: characterType) => {};
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  };
 
   return (
-    <div>
-      <div>
-        <img
-          style={{ width: "60px", height: "55px" }}
-          alt=""
-          src={getData?.image}
-        />
-        <div style={{ display: "flex" }}>
-          <h2 style={{ marginRight: "10px" }}>Name :</h2>
-          <p>{getData?.name}</p>
-        </div>
-        <div style={{ display: "flex" }}>
-          <h2 style={{ marginRight: "10px" }}>Gender :</h2>
-          <p>{getData?.gender}</p>
-        </div>
-        <div style={{ display: "flex" }}>
-          <h2 style={{ marginRight: "10px" }}>Species :</h2>
-          <p>{getData?.species}</p>
-        </div>
-        <div style={{ display: "flex" }}>
-          <h2 style={{ marginRight: "10px" }}>created :</h2>
-          <p>{getData?.created}</p>
-        </div>
-        <div style={{ display: "flex" }}>
-          <h2 style={{ marginRight: "10px" }}>Status :</h2>
-          <p>{getData?.status}</p>
-        </div>
-        <div style={{ display: "flex" }}>
-          <h2 style={{ marginRight: "10px" }}>Location :</h2>
-          <p>{getData?.location.name}</p>
-        </div>
-        <div style={{ display: "flex" }}>
-          <h2 style={{ marginRight: "10px" }}>Episode Count:</h2>
-          <p>{getData?.episodeCount}</p>
+    <div className="details">
+      <div className="character-detail">
+        <img alt="" src={getData?.image} />
+        <div>
+          {" "}
+          <div className="character-item">
+            <h3 style={{ marginRight: "10px" }}>Name :</h3>
+            <p>{getData?.name}</p>
+          </div>
+          <div className="character-item">
+            <h3 style={{ marginRight: "10px" }}>Gender :</h3>
+            <p>{getData?.gender}</p>
+          </div>
+          <div className="character-item">
+            <h3 style={{ marginRight: "10px" }}>Species :</h3>
+            <p>{getData?.species}</p>
+          </div>
+          <div className="character-item">
+            <h3 style={{ marginRight: "10px" }}>created :</h3>
+            <p>{getData?.created.substring(0, 10)}</p>
+          </div>
+          <div className="character-item">
+            <h3 style={{ marginRight: "10px" }}>Status :</h3>
+            <p>{getData?.status}</p>
+          </div>
+          <div className="character-item">
+            <h3 style={{ marginRight: "10px" }}>Location :</h3>
+            <p>{getData?.location.name}</p>
+          </div>
+          <div className="character-item">
+            <h3 style={{ marginRight: "10px" }}>Episode Count:</h3>
+            <p>{getData?.episodeCount}</p>
+          </div>
         </div>
       </div>
     </div>
--- types/index.ts
+++ types/index.ts
@@ -4,3 +4,19 @@ type ItemType = {
   status: string;
   id: number;
 };
+
+type location = {
+  name: string;
+};
+
+type characterType = {
+  name: string;
+  image: string;
+  status: string;
+  id: number;
+  species: string;
+  created: string;
+  gender: string;
+  location: location;
+  episodeCount: number;
+};
